80109eb6892d9bea7159315fa9ccdf6916f5079e
Geovani <geovanir.dev@gmail.com>
2026-08-22 19:08:09 -0300

    revert: fault-injection build failure amostra 008

--- fault-injection/inject_build_failure.sh
+++ fault-injection/inject_build_failure.sh
@@ -23,7 +23,7 @@ case $VARIANT in
   3) sed -i '0,/"name"/{s/"name"/"name",/}' $PKG ;;
   4) sed -i 's|"@grpc/grpc-js"|"@grpc/grpc-j"|' $PKG ;;
   5) cp $SERVICE/Dockerfile /tmp/Dockerfile.backup
-     sed -i 's/node:[0-9.]*-alpine@sha256:[a-f0-9]*/node:inexistente-99-alpine/' $SERVICE/Dockerfile ;;
+     sed -i 's/node:[^ ]*-alpine[^ ]*/node:inexistente-99-alpine/' $SERVICE/Dockerfile ;;
 esac
 
 git add $PKG $SERVICE/Dockerfile
